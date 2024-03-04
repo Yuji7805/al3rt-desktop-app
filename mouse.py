836eb5c927b6ee4ab0eeeb0dc8897d72e30451ea
Yuji Koyama <yujikoyama485@gmail.com>
2024-03-04 17:52:22 +1000

    fix: remove scroll, move events

--- mouse.py
+++ mouse.py
@@ -1,8 +1,7 @@
 from pynput.mouse import Listener
 
-
-def on_move(x, y):
-    print(f'Mouse moved to ({x}, {y})')
+start_x = None
+start_y = None
 
 
 def on_click(x, y, button, pressed):
@@ -11,17 +10,12 @@ def on_click(x, y, button, pressed):
     if pressed:
         start_x, start_y = x, y
     else:
-        on_select(start_x, start_y, x, y)
-    # print(action)
-    # print(f'Mouse {action} at ({x}, {y}) with {button}')
+        if start_x != x and start_y != y:
+            on_select(start_x, start_y, x, y)
 
 
-def on_select(x1, y1, x2, y2):
+def on_select():
     print("Text selected")
-
-
-# Variables to store the starting and ending positions of the selection
-start_x, start_y = None, None
 
 
 # Start listening to mouse events
