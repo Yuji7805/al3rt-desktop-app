ee86ae7537e1829d90747d3c4e11d019b035590a
Yuji Koyama <yujikoyama485@gmail.com>
2024-03-04 18:18:54 +1000

    feat: add clipboard feature

--- mouse.py
+++ mouse.py
@@ -1,4 +1,5 @@
 from pynput.mouse import Listener
+import pyperclip
 
 start_x = None
 start_y = None
@@ -11,11 +12,17 @@ def on_click(x, y, button, pressed):
         start_x, start_y = x, y
     else:
         if start_x != x and start_y != y:
-            on_select(start_x, start_y, x, y)
+            on_select()
 
 
 def on_select():
-    print("Text selected")
+    import pyautogui
+
+    # Simulate Ctrl + C to copy the selected text to the clipboard
+    pyautogui.hotkey('ctrl', 'c')
+
+    selected_text = pyperclip.paste()
+    print("Selected text:", selected_text)
 
 
 # Start listening to mouse events
